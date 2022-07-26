867f4746e6876d1f0a7dbf24fddc368b313a9034
thaimynguyen <thaimynguyen@gmail.com>
2022-07-26 15:13:17 -0700

    Add mock data and add function to validate & parse data with coordinates
    Render chart component with mock data

--- src/components/Chart.js
+++ src/components/Chart.js
@@ -1,9 +1,19 @@
 import React from "react";
+import "./Chart.css";
 
-const Chart = () => {
+const Chart = (props) => {
+  const chartJSX = (data) =>
+    data.map((row) => {
+      const chartRow = row.map((element) => (
+        <div className="stitch">{element.stitch_type}</div>
+      ));
+      return <div className="chart-row">{chartRow}</div>;
+    });
+
   return (
     <div>
       <h2>Chart</h2>
+      <div className="chart">{chartJSX(props.data)}</div>
     </div>
   );
 };
