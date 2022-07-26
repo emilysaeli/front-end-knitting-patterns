bd0c7f6c4d68e725b50c4d5f627bf887d86f355a
thaimynguyen <thaimynguyen@gmail.com>
2022-07-26 16:29:51 -0700

    display chart

--- src/components/Chart.js
+++ src/components/Chart.js
@@ -1,9 +1,19 @@
 import React from "react";
+import data from "../services/mockData.json";
+import "./Chart.css";
 
 const Chart = () => {
   return (
     <div>
       <h2>Chart</h2>
+      <div className="chart">
+        {data.map((row) => {
+          const chartRow = row.map((element) => (
+            <div className="stitch">{element.stitch_type}</div>
+          ));
+          return <div className="chart-row">{chartRow}</div>;
+        })}
+      </div>
     </div>
   );
 };
