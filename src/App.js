45eeffbc2757e7ec22da23cd2fc914c8a925bb69
Grace <gpark439@gmail.com>
2022-07-29 10:50:27 -0700

    GP: Get request API call to back end

--- src/App.js
+++ src/App.js
@@ -7,12 +7,26 @@ import Legend from "./components/Legend";
 import data from "./services/patterns/02_reverse_ridge/output_reverse_ridge.json";
 // import data from "./services/patterns/03_diagonal_seed/output_diagonal_seed.json";
 // import data from "./services/patterns/04_tile_squares/output_tile_squares.json";
+import { useState } from "react";
+import axios from "axios";
 
+import React from "react";
 const App = () => {
+  const URL = "https://pattern-handler-test-api.herokuapp.com";
+  const [chartData, setChartData] = useState([]);
+
   // send pattern form input to back-end
   const submitPattern = (patternForm) => {
     console.log("submitPattern called");
-    console.log(patternForm);
+
+    axios
+      .get(`${URL}`)
+      .then((response) => {
+        setChartData(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   };
 
   return (
@@ -24,7 +38,7 @@ const App = () => {
         <Form submitPattern={submitPattern}></Form>
       </section>
       <section className="legend">
-        <Legend></Legend>
+        <Legend data={data}></Legend>
       </section>
       <section>
         <Chart data={data}></Chart>
